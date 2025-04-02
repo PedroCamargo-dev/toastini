c37a4fcfb92b18be93f7236aa83cd97109698349
Pedro Camargo <pedrocamargo3008@gmail.com>
2025-04-01 22:59:53 -0300

    config: add Jest setup and update testing scripts

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/context/(.*)$': '<rootDir>/src/context/$1',
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@/types/(.*)$': '<rootDir>/src/types/$1',
+    '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+}
